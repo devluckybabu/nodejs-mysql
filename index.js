5d73a2050ce47d5319f871553ec054a40875b759
devluckybabu <dev.luckybabu@gmail.com>
2022-01-21 13:32:59 +0530

    update

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const connection = require("./lib/connection");
 const deleteData = require("./lib/delete_data");
-const getData = require("./lib/get_data");
 const insertData = require("./lib/insert_data");
 const updateData = require("./lib/update_data");
+const getMethod = require("./lib/v2.0/get_method");
 
 class Connection {
   #connection;
   constructor({ host, user, password, database, debug, port }) {
     this.#connection = connection.createPool({ host, user, password, database, debug, port });
   };
-  get = (table, id) => getData({ connection: this.#connection, table, id });
+  get = (table) => new getMethod({ connection: this.#connection, table })
   set = (table, data) => insertData({ connection: this.#connection, data, table });
   update = (table, data, id) => updateData({ connection: this.#connection, data, table, id });
   delete = (table, id) => deleteData({ connection: this.#connection, table, id });
